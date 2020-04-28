f331a8ff9870054dafb0cdd617ec03b53c40a6eb
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-04-27 21:30:43 -0600

    Added api call for Spotify

--- liri.js
+++ liri.js
@@ -1,26 +1,43 @@
 require("dotenv").config();
-
 var axios = require("axios");
-
 var fs = require("fs");
-var fsdata = fs.readFileSync("/random.txt");
+var nodeArgs = process.argv;
 
 var Spotify = require("node-spotify-api")
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
-var artistQueryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=trilogy"
-var artistName = "Bad Omens";
-
-var songQueryUrl =
 var songName = "";
 
-var movieQueryUrl =
-var movieName = "";
+for (var i = 2; i < nodeArgs.length; i++) {
 
-axios
-    .get(artistQueryUrl)
+  if (i > 2 && i < nodeArgs.length) {
+    songName = songName + "+" + nodeArgs[i];
+  } else {
+    songName += nodeArgs[i];
+  }
+}
+spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  } 
+console.log(JSON.stringify(data.tracks.items[0].artists[0].name));
+console.log(JSON.stringify(data.tracks.items[0].name));
+console.log(JSON.stringify(data.tracks.items[0].preview_url));
+console.log(JSON.stringify(data.tracks.items[0].album.name));
+});
 
-.then(function(response) {
-  console.log(response.data)
-})
+
+
+
+
+
+
+
+
+
+// var fsdata = fs.readFileSync("/random.txt");
+// var artistQueryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=trilogy"
+// var artistName = "Bad Omens";
+// var movieQueryUrl =
+// var movieName = "";
