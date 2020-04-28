d0663d9bee4dada63a9e937d25b23c6f688bda24
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-04-27 21:41:18 -0600

    fixed the node spotify call

--- liri.js
+++ liri.js
@@ -9,9 +9,8 @@ var spotify = new Spotify(keys.spotify);
 
 var songName = "";
 
-for (var i = 2; i < nodeArgs.length; i++) {
-
-  if (i > 2 && i < nodeArgs.length) {
+for (var i = 3; i < nodeArgs.length; i++) {
+  if (i > 3 && i < nodeArgs.length) {
     songName = songName + "+" + nodeArgs[i];
   } else {
     songName += nodeArgs[i];
