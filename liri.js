6168f488e5b63012123563cde49c8b8e0a489419
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-04-28 14:25:28 -0600

    Added js

--- liri.js
+++ liri.js
@@ -1,12 +1,28 @@
 require("dotenv").config();
-var axios = require("axios");
+//var axios = require("axios");
 var fs = require("fs");
 var nodeArgs = process.argv;
 
+var data =
+fs.readFile("random.txt", "utf8", function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  data = data.split(", ");
+
+  if (data === "spotify-this-song") {
+    songInfo()
+  } else if (data === "concert-this"){
+    bandInfo()
+  } else if (data === "movie-this"){
+    movieInfo
+  }
+  
+
+function songInfo() {
 var Spotify = require("node-spotify-api")
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-
 var songName = "";
 
 for (var i = 3; i < nodeArgs.length; i++) {
@@ -25,18 +41,28 @@ console.log(JSON.stringify(data.tracks.items[0].name));
 console.log(JSON.stringify(data.tracks.items[0].preview_url));
 console.log(JSON.stringify(data.tracks.items[0].album.name));
 });
+};
+
+function bandTown(){
+
+  var bandName = "";
+
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      bandName = bandName + "+" + nodeArgs[i];
+    } else {
+      bandName += nodeArgs[i];
+    }
+}
+var queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=trilogy"
 
 
 
+console.log(queryUrl)
+
+function movieInfo(){
 
 
+}
 
-
-
-
-
-// var fsdata = fs.readFileSync("/random.txt");
-// var artistQueryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=trilogy"
-// var artistName = "Bad Omens";
-// var movieQueryUrl =
-// var movieName = "";
+}});
